82f918292033814287f01803c04222ff45e5c130
Cameron Mease <cameron@pinknose.net>
2019-12-15 22:51:49 -0500

    Added more tests for distributions.

--- QrtUnitTests/SerializationTests.cs
+++ QrtUnitTests/SerializationTests.cs
@@ -12,7 +12,7 @@ namespace Pinknose.QuantitativeRiskToolkit.Tests
     public class SerializationTests
     {
         [TestMethod()]
-        public void SimpleDistribution()
+        public void DiscreteUniformDistribution()
         {
             Simulation.NumberOfSamples = 5;
             JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
@@ -34,11 +34,132 @@ namespace Pinknose.QuantitativeRiskToolkit.Tests
             Assert.AreEqual(val.ContrainedToInt, newVal.ContrainedToInt);
             Assert.AreEqual(val.RandomSeed, newVal.RandomSeed);
             Assert.AreEqual(val.Min.ScalarValue, newVal.Min.ScalarValue);
-            //Assert.AreEqual(val.Minimum, newVal.Minimum);
-            //Assert.AreEqual(val.Max.ScalarValue, 5);
-            //Assert.AreEqual(val.Maximum, 5);
-            //Assert.AreEqual(val.Mean, 3.4);
-            //Assert.AreEqual(val.Name, name);
+            Assert.AreEqual(val.Minimum, newVal.Minimum);
+            Assert.AreEqual(val.Max.ScalarValue, 5);
+            Assert.AreEqual(val.Maximum, 5);
+            Assert.AreEqual(val.Mean, 3.4);
+            Assert.AreEqual(val.Name, name);
+        }
+
+        [TestMethod()]
+        public void ContinuousUniformDistribution()
+        {
+            Simulation.NumberOfSamples = 5;
+            JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
+
+            string name = "My Distribution";
+
+            var val = new ContinuousUniformDistribution(1.0, 5.0, 0)
+            {
+                Name = name
+            };
+
+            Guid guid = val.Guid;
+
+            string json = JObject.FromObject(val, serializer).ToString();
+            Simulation.ClearDistributionList();
+            var newVal = JObject.Parse(json).ToObject<ContinuousUniformDistribution>(serializer);
+
+            Assert.AreEqual(val.Guid, newVal.Guid);
+            Assert.AreEqual(val.ContrainedToInt, newVal.ContrainedToInt);
+            Assert.AreEqual(val.RandomSeed, newVal.RandomSeed);
+            Assert.AreEqual(val.Min.ScalarValue, newVal.Min.ScalarValue);
+            Assert.AreEqual(val.Minimum, newVal.Minimum);
+            Assert.AreEqual(val.Max.ScalarValue, 5);
+            Assert.AreEqual(val.Maximum, newVal.Maximum);
+            Assert.AreEqual(val.Mean, newVal.Mean);
+            Assert.AreEqual(val.Name, name);
+        }
+
+        [TestMethod()]
+        public void BernoulliDistribution()
+        {
+            Simulation.NumberOfSamples = 5;
+            JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
+
+            string name = "My Distribution";
+
+            var val = new BernoulliDistribution(0.75, 0)
+            {
+                Name = name
+            };
+
+            Guid guid = val.Guid;
+
+            string json = JObject.FromObject(val, serializer).ToString();
+            Simulation.ClearDistributionList();
+            var newVal = JObject.Parse(json).ToObject<BernoulliDistribution>(serializer);
+
+            Assert.AreEqual(val.Guid, newVal.Guid);
+            Assert.AreEqual(val.ContrainedToInt, newVal.ContrainedToInt);
+            Assert.AreEqual(val.RandomSeed, newVal.RandomSeed);
+            Assert.AreEqual(val.Probability.ScalarValue, newVal.Probability.ScalarValue);
+            Assert.AreEqual(val.Minimum, newVal.Minimum);
+            Assert.AreEqual(val.Maximum, newVal.Maximum);
+            Assert.AreEqual(val.Mean, newVal.Mean);
+            Assert.AreEqual(val.Name, name);
+        }
+
+        [TestMethod()]
+        public void PertDistribution()
+        {
+            Simulation.NumberOfSamples = 5;
+            JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
+
+            string name = "My Distribution";
+
+            var val = new PertDistribution(1.0, 5.0, 10.0, 0)
+            {
+                Name = name
+            };
+
+            Guid guid = val.Guid;
+
+            string json = JObject.FromObject(val, serializer).ToString();
+            Simulation.ClearDistributionList();
+            var newVal = JObject.Parse(json).ToObject<PertDistribution>(serializer);
+
+            Assert.AreEqual(val.Guid, newVal.Guid);
+            Assert.AreEqual(val.ContrainedToInt, newVal.ContrainedToInt);
+            Assert.AreEqual(val.RandomSeed, newVal.RandomSeed);
+            Assert.AreEqual(val.Min.ScalarValue, newVal.Min.ScalarValue);
+            Assert.AreEqual(val.MostLikely.ScalarValue, newVal.MostLikely.ScalarValue);
+            Assert.AreEqual(val.Max.ScalarValue, newVal.Max.ScalarValue);
+            Assert.AreEqual(val.Minimum, newVal.Minimum);
+            Assert.AreEqual(val.Maximum, newVal.Maximum);
+            Assert.AreEqual(val.Mean, newVal.Mean);
+            Assert.AreEqual(val.Name, name);
+        }
+
+        [TestMethod()]
+        public void BinomialDistribution()
+        {
+            /*
+            Simulation.NumberOfSamples = 5;
+            JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
+
+            string name = "My Distribution";
+
+            var val = new BinomialDistribution(, 0)
+            {
+                Name = name
+            };
+
+            Guid guid = val.Guid;
+
+            string json = JObject.FromObject(val, serializer).ToString();
+            Simulation.ClearDistributionList();
+            var newVal = JObject.Parse(json).ToObject<BinomialDistribution>(serializer);
+
+            Assert.AreEqual(val.Guid, newVal.Guid);
+            Assert.AreEqual(val.ContrainedToInt, newVal.ContrainedToInt);
+            Assert.AreEqual(val.RandomSeed, newVal.RandomSeed);
+            Assert.AreEqual(val.Probability.ScalarValue, newVal.Probability.ScalarValue);
+            Assert.AreEqual(val.Minimum, newVal.Minimum);
+            Assert.AreEqual(val.Maximum, newVal.Maximum);
+            Assert.AreEqual(val.Mean, newVal.Mean);
+            Assert.AreEqual(val.Name, name);
+            */
         }
 
         [TestMethod()]
