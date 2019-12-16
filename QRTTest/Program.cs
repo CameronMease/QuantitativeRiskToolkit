a02ea00dae98845515f808315372614ada39d815
Cameron Mease <cameron@pinknose.net>
2019-12-15 22:57:29 -0500

    Latest version of test code.

--- QRTTest/Program.cs
+++ QRTTest/Program.cs
@@ -19,13 +19,20 @@ namespace QRTTest
 
             JsonSerializer serializer = JsonSerializer.Create(JsonSettings.SerializerSettings);
 
-            var val = new DiscreteUniformDistribution(1, 5, 0);
+            var d1 = new DiscreteUniformDistribution(1,5, 0);
+            var d2 = new DiscreteUniformDistribution(6, 10, 0);
+            var d3 = new DiscreteUniformDistribution(d1, d2, 0);
 
-            string json = JObject.FromObject(val, serializer).ToString();
+            string jsonD1 = JObject.FromObject(d1, serializer).ToString();
+            string jsonD2 = JObject.FromObject(d2, serializer).ToString();
+            string jsonD3 = JObject.FromObject(d3, serializer).ToString();
 
             Simulation.ClearDistributionList();
-            
-            var newVal = JObject.Parse(json).ToObject<DiscreteUniformDistribution>();
+
+            var newD1 = JObject.Parse(jsonD1).ToObject<DiscreteUniformDistribution>(serializer);
+            var newD2 = JObject.Parse(jsonD2).ToObject<DiscreteUniformDistribution>(serializer);
+            var newVal = JObject.Parse(jsonD3).ToObject<DiscreteUniformDistribution>(serializer);
+            var duhh = newVal.Min.DistributionValue;
           
         }
 
